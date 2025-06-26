a464e8fa5c42a6244687b7840c4063eb31ae86c4
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-06-26 20:16:16 +0200

    Create Solution.kt

---
+++ Solution.kt
@@ -0,0 +1,19 @@
+
+class Solution {
+
+    fun divideString(input: String, groupSize: Int, fill: Char): Array<String> {
+        val groups = Array<String>((input.length + groupSize - 1) / groupSize) { String() }
+        var index = 0
+
+        for (i in input.indices step groupSize) {
+            groups[index] = input.substring(i, min(i + groupSize, input.length))
+            ++index
+        }
+
+        if (groups[index - 1].length < groupSize) {
+            groups[index - 1] += fill.toString().repeat(groupSize - groups[index - 1].length)
+        }
+
+        return groups
+    }
+}
